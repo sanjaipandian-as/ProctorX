63b130d7c64cd754edaef08f824dbca34b1b0eb1
sanjaipandi66@gmail.com <sanjaipandi66@gmail.com>
2025-11-03 08:54:36 +0530

    done

--- Front-End/Api.js
+++ Front-End/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://proctorx-03u1.onrender.com",
+  baseURL: "https://proctor-x-1685.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
